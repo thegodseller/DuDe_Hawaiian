c3d9036d631e72b966df5afaa9a621c27013dc02
ramnique <30795890+ramnique@users.noreply.github.com>
2025-02-03 20:55:58 +0530

    auth0: always show univeseral login

--- apps/rowboat/app/api/auth/[auth0]/route.ts
+++ apps/rowboat/app/api/auth/[auth0]/route.ts
@@ -1,3 +1,10 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+// pages/api/auth/[auth0].js
+import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 
-export const GET = handleAuth();
+export const GET = handleAuth({
+  login: handleLogin({
+    authorizationParams: {
+        prompt: 'login'
+    }
+  })
+});
